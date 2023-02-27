0fd8bdb5d689372f232caef2ed0820b8ad05d965
AlucarD <skoriy12990@gmail.com>
2023-02-27 19:16:04 +0300

    add setProfileStatus thunk

--- src/redux/thunks/profileThunk.ts
+++ src/redux/thunks/profileThunk.ts
@@ -1,5 +1,7 @@
 import { DispatchType } from '../_store'
+import { getProfileStatus } from './../../API/api'
 import { getUserProfile } from '../../API/api'
+import { setProfileStatusAC } from './../actions/profilePageActions'
 import { setUserProfileAC } from '../actions/profilePageActions'
 import { toggleIsLoadingAC } from '../actions/appActions'
 
@@ -11,6 +13,17 @@ export const setUserProfileThunk = (userId: number) => {
         dispatch(setUserProfileAC(data!))
         setTimeout(() => dispatch(toggleIsLoadingAC(false)), 300)
       })
-      .catch(() => dispatch(toggleIsLoadingAC(false)))
+      .catch((error) => console.warn(error))
+      .finally(() => dispatch(toggleIsLoadingAC(false)))
+  }
+}
+
+export const setProfileStatusThunk = (userId: number) => {
+  return (dispatch: DispatchType) => {
+    dispatch(toggleIsLoadingAC(true))
+    getProfileStatus(userId)
+      .then((data) => dispatch(setProfileStatusAC(data!)))
+      .catch((error) => console.warn(error))
+      .finally(() => dispatch(toggleIsLoadingAC(false)))
   }
 }
